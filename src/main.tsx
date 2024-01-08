71938be0b83b00b16613fa01b02b681ab2d1882c
osamu-saito-23 <osamu.saito@txpmedical.com>
2024-01-08 19:52:20 +0900

    add dependencies and show react-flow background

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ReactFlowProvider } from "reactflow";
+import "./index.css";
+import "reactflow/dist/style.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlowProvider>
+        <App />
+      </ReactFlowProvider>
+    </div>
+  </React.StrictMode>
+);
